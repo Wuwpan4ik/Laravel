c399427b622eff5382a2d165a0113ead47640fc6
Wuwpan4ik <dimalim110@gmail.com>
2022-03-12 21:56:45 +0400

    editStructure

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -1,52 +1,78 @@
 @if ($comments)
     @foreach($comments as $note => $category)
         @if (!$category->parent_id)
-            <div class="profile__note">
-                <div class="profile__note-container">
-					<div class="profile__info">
-						<div class="profile__title">{{ $category->title }}</div>
-						<div class="profile__text">{{ $category->description }}</div>
-					</div>
-					<button class="profile__form-btn library__button" id="answer" title="Ответить">Ответить</button>
-                    <form class="profile__form profile__form-block" id="form" action="{{ route('comment-add', ['user_to' => $id, 'user_id' => Auth::user()->id, 'parent' => $category->id ]) }}" method="POST">
-                        @csrf
-                        <input type="text" name="title" class="profile__input" placeholder="Заголовок">
-                        <textarea name="comment" id="comment"></textarea>
-                        <button type="submit" class="profile__form-btn">Отправить</button>
-                    </form>
-                    @foreach($category->children as $item)
-                        <div class="profile__note profile__note-answer">
-                            <div class="profile__title">{{ $item->title }}</div>
-                            <div class="profile__text">'{{ $category->description }}' - {{ $item->description }}</div>
-                            @if ( $item->user_id == Auth::user()->id)
-                                <form class="form__delete" action="{{ route('comment-delete', ['note_id' => $category->id, 'user_to' => $category->user_to]) }}" method="POST">
+            <ul class="media-list">
+                <!-- Комментарий (уровень 1) -->
+                <li class="media media-top mb-1">
+                    <div class="media-body">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <img class="media-left-img media-object img-rounded" src="{{ url('image/i.jpg') }}" alt="...">
+                                <div class="author">{{ $category->title }}</div>
+                                <div class="metadata">
+                                    <span class="date">{{ $category->user_id }}</span>
+                                </div>
+                            </div>
+                            <div class="panel-body">
+                                <div class="media-text text-justify">{{ $category->description }}</div>
+                            </div>
+                            <div class="panel-footer">
+                                <button id="answer" class="btn btn-dark" href="#">Ответить</button>
+                                <form class="profile__form profile__form-block" id="form" action="{{ route('comment-add', ['user_to' => $id, 'user_id' => Auth::user()->id, 'parent' => $category->id ]) }}" method="POST">
                                     @csrf
-                                    <button title="Удалить комментарий" class="form__delete-btn"><i class="fa fa-times"></i></button>
+                                    <input type="text" name="title" class="profile__input" placeholder="Заголовок">
+                                    <textarea name="comment" id="comment"></textarea>
+                                    <button type="submit" class="profile__form-btn">Отправить</button>
                                 </form>
-                            @elseif ($id == Auth::user()->id)
-                                <form class="form__delete" action="{{ route('comment-delete', ['note_id' => $category->id, 'user_to' => $category->user_to]) }}" method="POST">
-                                    @csrf
-                                    <button title="Удалить комментарий" class="form__delete-btn"><i class="fa fa-times"></i></button>
-                                </form>
-                            @endif
+                            </div>
                         </div>
-                    @endforeach
+                        @if ( $category->user_id == Auth::user()->id)
+                            <form class="form__delete" action="{{ route('comment-delete', ['note_id' => $category->id, 'user_to' => $category->user_to]) }}" method="POST">
+                                @csrf
+                                <button title="Удалить комментарий" class="form__delete-btn"><i class="fa fa-times"></i></button>
+                            </form>
+                        @elseif ($id == Auth::user()->id)
+                            <form class="form__delete" action="{{ route('comment-delete', ['note_id' => $category->id, 'user_to' => $category->user_to]) }}" method="POST">
+                                @csrf
+                                <button title="Удалить комментарий" class="form__delete-btn"><i class="fa fa-times"></i></button>
+                            </form>
+                        @endif
+                        @foreach($category->children as $item)
+                        <!-- Ещё один вложенный медиа-компонент (уровень 2) -->
+                        <div class="media mb-2 mt-4 ml-4">
+                            <div class="media-body">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        <img class="media-left-img media-object img-rounded" src="{{ url('image/i.jpg') }}" alt="...">
+                                        <div class="author">{{ $item->title }}</div>
+                                        <div class="metadata">
+                                            <span class="date">{{ $category->user_id }}</span>
+                                        </div>
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="media-text text-justify">{{ $category->description }}</div>
+                                    </div>
+                                </div>
+                                @if ( $item->user_id == Auth::user()->id)
+                                    <form class="form__delete" action="{{ route('comment-delete', ['note_id' => $category->id, 'user_to' => $category->user_to]) }}" method="POST">
+                                        @csrf
+                                        <button title="Удалить комментарий" class="form__delete-btn"><i class="fa fa-times"></i></button>
+                                    </form>
+                                @elseif ($id == Auth::user()->id)
+                                    <form class="form__delete" action="{{ route('comment-delete', ['note_id' => $category->id, 'user_to' => $category->user_to]) }}" method="POST">
+                                        @csrf
+                                        <button title="Удалить комментарий" class="form__delete-btn"><i class="fa fa-times"></i></button>
+                                    </form>
+                                @endif
 
-                    {{--Удаление комментария--}}
-                    @if ( $category->user_id == Auth::user()->id)
-                        <form class="form__delete" action="{{ route('comment-delete', ['note_id' => $category->id, 'user_to' => $category->user_to]) }}" method="POST">
-                            @csrf
-                            <button title="Удалить комментарий" class="form__delete-btn"><i class="fa fa-times"></i></button>
-                        </form>
-                    @elseif ($id == Auth::user()->id)
-                        <form class="form__delete" action="{{ route('comment-delete', ['note_id' => $category->id, 'user_to' => $category->user_to]) }}" method="POST">
-                            @csrf
-                            <button title="Удалить комментарий" class="form__delete-btn"><i class="fa fa-times"></i></button>
-                        </form>
-                    @endif
-                    {{--                                Удаление комментария--}}
-                </div>
-            </div>
+                            </div>
+                        </div>
+                        <!-- Конец ещё одного вложенного комментария (уровень 2) -->
+                        @endforeach
+                    </div>
+                </li>
+                <!-- Конец комментария (уровень 1) -->
+            </ul>
         @endif
     @endforeach
 @endif
